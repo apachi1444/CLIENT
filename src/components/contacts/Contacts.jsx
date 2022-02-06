bda3d8dd3571274125e44cf832270dcf7e31c605
Medelouali <Mohammedelouali1999@gmail.com>
2022-02-06 03:09:23 -0800

    adding a logo

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import DraftsOutlinedIcon from '@mui/icons-material/DraftsOutlined';
-import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
+import React from "react";
+import DraftsOutlinedIcon from "@mui/icons-material/DraftsOutlined";
+import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
+import Logo from "./Logo";
+
 import "./contacts.css";
 
 function Contacts() {
-    return (
-        <div className='contacts'>
-            <div className="email">
-                <DraftsOutlinedIcon/>
-                <h6>someemailgoeshere@gmail.com</h6>
-            </div>
-            <div className="phone">
-                <LocalPhoneOutlinedIcon/>
-                <h6>+212712785477</h6>
-            </div>
+  return (
+    <div className="contacts">
+      <Logo fill={"blue"} background={"#e5f4f5"} />
+      <div className="phone-email">
+        <div className="email">
+          <DraftsOutlinedIcon />
+          <h6>someemailgoeshere@gmail.com</h6>
         </div>
-    )
+        <div className="phone">
+          <LocalPhoneOutlinedIcon />
+          <h6>+212712785477</h6>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contacts;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
+import Logo from "../contacts/Logo";
 import "./footer.css";
 
 function Footer() {
-    return (
-        <div className='footer'>
-            <p>© 2022, Colocakesh.com, Inc.</p>
-        </div>
-    )
+  return (
+    <div className="footer">
+      <div className="inner-footer">
+        <Logo fill={"pink"} background={"#0f0f0f"} />
+        <p>© 2022, Colocakesh.com, Inc.</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
