824b92c96a0079c9acf3741f1071585932adda48
yessine <yessinejawa@gmail.com>
2022-03-05 23:41:09 +0100

    add the coord file

--- src/components/home/main/locations/Locations.jsx
+++ src/components/home/main/locations/Locations.jsx
@@ -2,21 +2,22 @@ import React, { useState } from "react";
 import "./locations.css";
 import City from "../../../core/cities/city/City";
 import { v4 as uuid } from "uuid";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 import Mape from "./Map/Mape";
 // import coord from "./coord";
 
 function Locations() {
-  const [lat, setLat]=useState("");
-  const [long, setLong]=useState("");
-  const handleLat=(e)=>{
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+  const handleLat = (e) => {
     setLat(e?.target?.value);
   };
-  const handleLong=(e)=>{
+  const handleLong = (e) => {
     setLong(e?.target?.value);
-  }
+  };
   return (
-    <motion.div className="locations"
+    <motion.div
+      className="locations"
       initial={{ x: "100vw", opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
       transition={{ duration: 0.3 }}
@@ -53,11 +54,11 @@ function Locations() {
         <section className="MapDiv">
           <div className="Map">
             <Mape></Mape>
-            <iframe
+            {/* <iframe
               title="The map"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d217406.37638548054!2d-8.1479381904096!3d31.63454497560447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdafee8d96179e51%3A0x5950b6534f87adb8!2sMarrakech!5e0!3m2!1sfr!2sma!4v1645716826955!5m2!1sfr!2sma"
               className="theMap"
-            ></iframe>
+            ></iframe> */}
             <div className="HoverMap">
               <div className="offersHoverMap">
                 We Have the offer of 2000dh is still up
--- src/components/home/main/locations/Map/mapGraphics/MapGraphics.jsx
+++ src/components/home/main/locations/Map/mapGraphics/MapGraphics.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 
 import Graphic from "@arcgis/core/Graphic";
+import coord from "../../coord";
 const MapGraphics = ({ view }) => {
   const formRef = useRef(null);
   // const [zone, setZone] = useState({
@@ -8,51 +9,45 @@ const MapGraphics = ({ view }) => {
   //   address: "klsqjflk",
   // });
   useEffect(() => {
+    console.log(coord);
     const layer = view.map.findLayerById("glResult1");
     console.log(layer);
     if (layer) {
-      const graphic = new Graphic({
-        geometry: {
-          type: "point",
-          x: -8.0189574,
-          y: 31.6536553,
-        },
-        symbol: {
-          type: "simple-marker",
-          color: "red",
-          size: 35,
-        },
-        popupTemplate: {
-          title: "Sidi Abbad",
-          content: "Offers Here",
-        },
-        attributes: {
-          name: "yr",
-        },
+      coord.map((item) => {
+        const graphic = new Graphic({
+          geometry: {
+            type: "point",
+            x: item.x,
+            y: item.y,
+          },
+          symbol: {
+            type: "simple-marker",
+            color: "red",
+            size: 15,
+          },
+          popupTemplate: {
+            title: item.title,
+            content:
+              "<p><b> " +
+              " Number of Offers In This Place : " +
+              item.numberOfOffers +
+              "</b></p>" +
+              "<ul><li>" +
+              item.title +
+              "people are married</li>" +
+              "<li>" +
+              "have never married" +
+              "</li>" +
+              "<li>are divorced</li><ul>",
+          },
+          attributes: {
+            name: "yr",
+          },
+        });
+        layer.add(graphic);
       });
-      const graphic1 = new Graphic({
-        geometry: {
-          type: "point",
-          y: 31.6686551,
-          x: -8.0227455,
-        },
-        symbol: {
-          type: "simple-marker",
-          color: "red",
-          size: 35,
-        },
-        popupTemplate: {
-          title: "Jnane Ourad",
-          content: "Offers Here",
-        },
-        attributes: {
-          name: "yr",
-        },
-      });
-      layer.add(graphic1);
-      layer.add(graphic);
     }
-    view.ui.add(formRef.current, "top-right");
+    // view.ui.add(formRef.current, "top-right");
     view.on("click", (event) => {
       view.hitTest(event).then((rep) => {
         console.log("rep", rep);
@@ -63,11 +58,7 @@ const MapGraphics = ({ view }) => {
     });
   }, [view]);
   return (
-    <div
-      ref={formRef}
-      className="esri-widget"
-      style={{ margin: 2, padding: 20 }}
-    >
+    <div ref={formRef} className="esri-widget">
       {/* <input type="text" value={state.address} />
       <input type="text" value={state.name} /> */}
     </div>
