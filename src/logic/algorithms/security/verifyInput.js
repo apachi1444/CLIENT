003f6e9b455a67655108943179d895db73015ee5
Medelouali <Mohammedelouali1999@gmail.com>
2022-03-16 11:22:19 -0700

    fixing the maps warnings + adding socket.io-client setup

--- src/logic/algorithms/security/verifyInput.js
+++ src/logic/algorithms/security/verifyInput.js
@@ -1,16 +1,16 @@
-// import { isEmail } from 'validator';
+import { isEmail } from 'validator';
 
-// export function verifyInput(label, value){
-//     const passRegex=/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-//     switch(label.toLowerCase()){
-//         case 'email':
-//             if(isEmail(value)) return false
-//             return 'Incorrect email, please provide a valid one!';
-//         case 'password':
-//             if(passRegex.test(value)) return false;
-//             return 'Minimum eight characters, at least one letter and one number';
-//         default:
-//             return false;
+export function verifyInput(label, value){
+    const passRegex=/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    switch(label.toLowerCase()){
+        case 'email':
+            if(isEmail(value)) return false;
+            return 'This is not an email, please get a valid one!';
+        case 'password':
+            if(passRegex.test(value)) return false;
+            return 'Minimum eight characters, at least one letter and one number!';
+        default:
+            return false;
 
-//     };
-// }
+    };
+}
